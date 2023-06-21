0da0764a53d2f57e84003589cf975b77bdeaa6f3
Captluke2328 <125783320+develtechmon@users.noreply.github.com>
2023-06-22 03:18:46 +0800

    +added control to takeoff without gps

--- coral_project/Active_Track_Drone_Gesture_Controlled/v1_1/rpi/control_tab.py
+++ coral_project/Active_Track_Drone_Gesture_Controlled/v1_1/rpi/control_tab.py
@@ -18,7 +18,101 @@ class controlTab:
         self.MAX_YAW = 15  
           
         self.takeoff_alt = 1.3
-          
+    
+    # Takeoff without GPS
+    def armAndTakeoff_nogps(self,altitude):
+        ## Constant ##
+        DEFAULT_TAKEOFF_THRUST = 0.7
+        SMOOTH_TAKEOFF_THRUST = 0.6
+        
+        print("Basic pre-arm checks")
+        # Don't let the user try to arm until autopilot is ready
+        # If you need to disable the arming check,
+        # just comment it with your own responsibility.
+        while not self.vehicle.is_armable:
+            print(" Waiting for vehicle to initialise...")
+            time.sleep(1)
+        
+        print("Arming motors")
+        # Copter should arm in GUIDED_NOGPS mode
+        self.vehicle.mode = VehicleMode("GUIDED_NOGPS")
+        self.vehicle.armed = True
+    
+        while not self.vehicle.armed:
+            print("waiting for arming...")
+            time.sleep(1)
+            
+        print("Taking off!")
+        
+        thrust = DEFAULT_TAKEOFF_THRUST
+        while True:
+            current_altitude = self.vehicle.location.global_relative_frame.alt
+            print(" Altitude: %f  Desired: %f" %(current_altitude, altitude))
+            if current_altitude >= altitude*0.95: # Trigger just below target alt.
+                print("Reached target altitude")
+                break
+            elif current_altitude >= altitude*0.6:
+                thrust = SMOOTH_TAKEOFF_THRUST
+            self.set_attitude(thrust = thrust)
+            time.sleep(0.2)
+
+    def send_attitude_target(self, roll_angle = 0.0, pitch_angle = 0.0,yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,thrust = 0.5):
+        """
+        use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
+                    When one is used, the other is ignored by Ardupilot.
+        thrust: 0 <= thrust <= 1, as a fraction of maximum vertical thrust.
+                Note that as of Copter 3.5, thrust = 0.5 triggers a special case in
+                the code for maintaining current altitude.
+        """
+        if yaw_angle is None:
+            # this value may be unused by the vehicle, depending on use_yaw_rate
+            yaw_angle = self.vehicle.attitude.yaw
+        # Thrust >  0.5: Ascend
+        # Thrust == 0.5: Hold the altitude
+        # Thrust <  0.5: Descend
+        msg = self.vehicle.message_factory.set_attitude_target_encode(
+            0, # time_boot_ms
+            1, # Target system
+            1, # Target component
+            0b00000000 if use_yaw_rate else 0b00000100,
+            self.to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
+            0, # Body roll rate in radian
+            0, # Body pitch rate in radian
+            math.radians(yaw_rate), # Body yaw rate in radian/second
+            thrust  # Thrust
+        )
+        self.vehicle.send_mavlink(msg)
+        
+                
+    def set_attitude(self,roll_angle = 0.0, pitch_angle = 0.0, yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,thrust = 0.5, duration = 0):
+        self.send_attitude_target(roll_angle, pitch_angle,yaw_angle, yaw_rate, False, thrust) 
+        
+        start = time.time()
+        while time.time() - start < duration:
+            self.send_attitude_target(roll_angle, pitch_angle, yaw_angle, yaw_rate, False,thrust)
+            time.sleep(0.1)
+            # Reset attitude, or it will persist for 1s more due to the timeout
+            self.send_attitude_target(0, 0,0, 0, True,thrust)
+
+    def to_quaternion(self,roll = 0.0, pitch = 0.0, yaw = 0.0):
+        """
+        Convert degrees to quaternions
+        """
+        t0 = math.cos(math.radians(yaw * 0.5))
+        t1 = math.sin(math.radians(yaw * 0.5))
+        t2 = math.cos(math.radians(roll * 0.5))
+        t3 = math.sin(math.radians(roll * 0.5))
+        t4 = math.cos(math.radians(pitch * 0.5))
+        t5 = math.sin(math.radians(pitch * 0.5))
+
+        w = t0 * t2 * t4 + t1 * t3 * t5
+        x = t0 * t3 * t4 - t1 * t2 * t5
+        y = t0 * t2 * t5 + t1 * t3 * t4
+        z = t1 * t2 * t4 - t0 * t3 * t5
+
+        return [w, x, y, z]
+     
+    # Takeoff with GPS
     def armAndTakeoff(self,altitude):
         print("Setting ground speed to 3")
         self.vehicle.groundspeed = 3
